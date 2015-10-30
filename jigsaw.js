78c0028e0f7db2111fef553e3603000d3fb5e9d2
Chris Spradling <cjspradling@gmail.com>
2015-10-29 19:17:42 -0500

    Added comments to code

--- jigsaw.js
+++ jigsaw.js
@@ -1,13 +1,26 @@
+//////////////////////////////////////////////////////////////
+//  Shuffler for randomly arranging images from random set  //
+//////////////////////////////////////////////////////////////
+
+// Selector for one of three image sets
 var imgset = Math.floor (Math.random() * 3)+1;
+
+// Array pre-structure
 var arr = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
 
+// Randomizer to shuffle the order of elements in the index array
 function shuffle(arr) {
     for(var j, x, i = arr.length; i; j = Math.floor(Math.random() * i), x = arr[--i], arr[i] = arr[j], arr[j] = x);
-    return arr;}
+    return arr;
+}
 
+// Perform shuffle
 shuffle(arr);
+
+// Assign images to existing <img> slots in order defined by array
 for (var i = 0; i < 12; i++) {
-    document.getElementById((i+1).toString()).src = "./images" + imgset.toString() + "/img" + imgset.toString() + "-" + arr[i] + ".jpg";}
+    document.getElementById((i+1).toString()).src = "./images" + imgset.toString() + "/img" + imgset.toString() + "-" + arr[i] + ".jpg";
+}
 
 
 
@@ -17,42 +30,29 @@ for (var i = 0; i < 12; i++) {
 //////////////////////////////////////////////////////////////
 var diffX, diffY, theElement;
 
-
+// Provided code for handling grabbing/ungrabbing
 function grabber(event) {
-
     theElement = event.currentTarget;
-
     var posX = parseInt(theElement.style.left);
     var posY = parseInt(theElement.style.top);
-
     diffX = event.clientX - posX;
     diffY = event.clientY - posY;
-
     document.addEventListener("mousemove", mover, true);
     document.addEventListener("mouseup", dropper, true);
-
     event.stopPropagation();
     event.preventDefault();
-
 }
-
-
 function mover(event) {
-
     theElement.style.left = (event.clientX - diffX) + "px";
     theElement.style.top = (event.clientY - diffY) + "px";
-
     event.stopPropagation();
 }
-
-
 function dropper(event) {
 // Perform snap when mouse released
     snapper(event);
 
     document.removeEventListener("mouseup", dropper, true);
     document.removeEventListener("mousemove", mover, true);
-
     event.stopPropagation();
 }
 
